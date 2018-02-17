e9d0598fa214ac19f39c849a8be5243b64dc86fe
ttay <ttay@umich.edu>
2018-02-16 23:19:36 -0500

    Created testing rpn files

---
+++ rpn.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import operator
+
+operators = {
+        #This is a dictionary!
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv
+}
+
+def calculate(arg):
+    operations = []
+    for operand in arg.split():
+        try:
+            #This means that the operation is a number
+            operand = float(operand)
+            operations.append(operand)
+        except ValueError:
+            #Encountered a non-numeric
+            a2 = operations.pop()
+            a1 = operations.pop()
+            operations.append( operators[operand] (a1,a2) )
+    return operations.pop()
+
+def main():
+    while True:
+        print("Result: {0}".format(calculate(input("rpn calc> "))))
+
+if __name__ == '__main__':
+    main()
+
---
+++ test_rpn.py
@@ -0,0 +1,19 @@
+import unittest
+import rpn
+
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate("1 1 +")
+        self.assertEqual(2,result)
+    def test_subtract(self):
+        result = rpn.calculate("4 6 -")
+        self.assertEqual(-2,result)
+    def test_multiply(self):
+        result = rpn.calculate("5 7 *")
+        self.assertEqual(35,result)
+    def test_negative_multiply(self):
+        result = rpn.calculate("-3 5 *")
+        self.assertEqual(-15,result)
+    def test_divide(self):
+        result = rpn.calculate("7 5 /")
+        self.assertEqual(1.4,result)
